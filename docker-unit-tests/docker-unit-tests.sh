9fb9fdc9209f927f309917e7196108e9c22ef5de
Vlad <vlad@gravitview.com>
2020-10-20 19:56:17 -0400

    Escape all paths
    Fix #1

--- docker-unit-tests/docker-unit-tests.sh
+++ docker-unit-tests/docker-unit-tests.sh
@@ -33,7 +33,7 @@ END
 
 ######################### Helpers
 _abort() {
-	local _ret="${2:-1}"
+	_ret="${2:-1}"
 	echo "$1" >&2
 	exit "${_ret}"
 }
@@ -48,69 +48,69 @@ _do_preflight_check() {
 }
 
 _run_docker_compose() {
-  if [ ! -f $WP_51_TESTS_DIR/wp-tests-config.php ] || [ ! -f $WP_LATEST_TESTS_DIR/wp-tests-config.php ]; then
+  if [ ! -f "$WP_51_TESTS_DIR/wp-tests-config.php" ] || [ ! -f "$WP_LATEST_TESTS_DIR/wp-tests-config.php" ]; then
     _abort "Run \"configure_test_suites\" before running tests"
   fi
 
-  local PHPUNIT_CONFIG="$PLUGIN_DIR/phpunit.xml"
-  local PHPUNIT_CONFIG_ALT="$PLUGIN_DIR/phpunit.xml.dist"
+  PHPUNIT_CONFIG="$PLUGIN_DIR/phpunit.xml"
+  PHPUNIT_CONFIG_ALT="$PLUGIN_DIR/phpunit.xml.dist"
 
-  if [ ! -f PHPUNIT_CONFIG ] && [ -f $PHPUNIT_CONFIG_ALT ]; then
-    PHPUNIT_CONFIG=$PHPUNIT_CONFIG_ALT
-  elif [ ! -f $PHPUNIT_CONFIG ]; then
+  if [ ! -f "$PHPUNIT_CONFIG" ] && [ -f "$PHPUNIT_CONFIG_ALT" ]; then
+    PHPUNIT_CONFIG="$PHPUNIT_CONFIG_ALT"
+  elif [ ! -f "$PHPUNIT_CONFIG" ]; then
     _abort "\"$PHPUNIT_CONFIG\" is not found"
   fi
 
-  ENV="-e $(grep -v '^#' .env | awk '{print}' ORS=' -e ')"
+  ENV="$([[ -f '.env' ]] && grep -v '^#' .env | awk 'ORS=" -e "' | sed 's/-e $//')"
 
   (export \
-     PLUGIN_DIR=$PLUGIN_DIR \
-     GF_PLUGIN_DIR=$GF_PLUGIN_DIR \
-     PHPUNIT_DIR=$PHPUNIT_DIR \
-     WP_51_TESTS_DIR=$WP_51_TESTS_DIR \
-     WP_LATEST_TESTS_DIR=$WP_LATEST_TESTS_DIR ;\
-   docker-compose -f $SCRIPT_DIR/docker-compose.yml run $ENV --rm $1 -c $PHPUNIT_CONFIG $2\
+     PLUGIN_DIR="$PLUGIN_DIR" \
+     GF_PLUGIN_DIR="$GF_PLUGIN_DIR" \
+     PHPUNIT_DIR="$PHPUNIT_DIR" \
+     WP_51_TESTS_DIR="$WP_51_TESTS_DIR" \
+     WP_LATEST_TESTS_DIR="$WP_LATEST_TESTS_DIR" ;\
+   docker-compose -f $SCRIPT_DIR/docker-compose.yml run -e "$ENV" --rm "${@}" -c "$PHPUNIT_CONFIG" \
   ) || _abort "Docker exited with an error"
 }
 
 ######################### Available commands
 configure_test_suites() {
-  if [ ! -f $WP_51_TESTS_DIR/wp-tests-config-sample.php ] || [ ! -f $WP_LATEST_TESTS_DIR/wp-tests-config-sample.php ]; then
+  if [ ! -f "$WP_51_TESTS_DIR/wp-tests-config-sample.php" ] || [ ! -f "$WP_LATEST_TESTS_DIR/wp-tests-config-sample.php" ]; then
     _abort "Run \"download_test_suites\" before configuring them"
   fi
 
  	# portable in-place argument for both GNU sed and Mac OSX sed (taken from WP's script)
 	if [[ $(uname -s) == 'Darwin' ]]; then
-		local ioption='-i.bak'
+		ioption='-i.bak'
 	else
-		local ioption='-i'
+		ioption='-i'
 	fi
 
-  TEST_SUITES=($WP_51_TESTS_DIR $WP_LATEST_TESTS_DIR)
+  TEST_SUITES=("$WP_51_TESTS_DIR" "$WP_LATEST_TESTS_DIR")
 
-  for test_suites in ${TEST_SUITES[*]}
+  for test_suites in "${TEST_SUITES[@]}"
   do
-    cp $test_suites/wp-tests-config-sample.php $test_suites/wp-tests-config.php
+    cp "$test_suites/wp-tests-config-sample.php" "$test_suites/wp-tests-config.php"
 
-    sed $ioption "s/'youremptytestdbnamehere'/getenv('MYSQL_DATABASE')/" "$test_suites"/wp-tests-config.php
-    sed $ioption "s/'yourusernamehere'/getenv('MYSQL_USER')/" "$test_suites"/wp-tests-config.php
-	  sed $ioption "s/'yourpasswordhere'/getenv('MYSQL_PASSWORD')/" "$test_suites"/wp-tests-config.php
-	  sed $ioption "s/'localhost'/getenv('MYSQL_HOST')/" "$test_suites"/wp-tests-config.php
+    sed $ioption "s/'youremptytestdbnamehere'/getenv('MYSQL_DATABASE')/" "$test_suites/wp-tests-config.php"
+    sed $ioption "s/'yourusernamehere'/getenv('MYSQL_USER')/" "$test_suites/wp-tests-config.php"
+	  sed $ioption "s/'yourpasswordhere'/getenv('MYSQL_PASSWORD')/" "$test_suites/wp-tests-config.php"
+	  sed $ioption "s/'localhost'/getenv('MYSQL_HOST')/" "$test_suites/wp-tests-config.php"
   done
 
 	echo "SUCCESS: test suites configured"
 }
 
 download_test_suites() {
-  local WP_51_HASH=$(wget --header="Authorization: token $GH_AUTH_TOKEN" -O - -q https://api.github.com/repos/WordPress/wordpress-develop/tags | jq 'map( select( .name | startswith( "5.1" ) ) ) | first | .commit.sha' | tr -d '"')
-  local WP_LATEST_HASH=$(wget --header="Authorization: token $GH_AUTH_TOKEN" -O - -q https://api.github.com/repos/WordPress/wordpress-develop/tags | jq '.[0].commit.sha' | tr -d '"')
+  WP_51_HASH=$(wget --header="Authorization: token $GH_AUTH_TOKEN" -O - -q https://api.github.com/repos/WordPress/wordpress-develop/tags | jq 'map( select( .name | startswith( "5.1" ) ) ) | first | .commit.sha' | tr -d '"')
+  WP_LATEST_HASH=$(wget --header="Authorization: token $GH_AUTH_TOKEN" -O - -q https://api.github.com/repos/WordPress/wordpress-develop/tags | jq '.[0].commit.sha' | tr -d '"')
 
-  if [ -f $WP_51_TESTS_DIR/.cache_hash ] && [ "$WP_51_HASH" == "$(head -n 1 $WP_51_TESTS_DIR/.cache_hash )" ] && [ "$FORCE_DOWNLOAD" != true ]; then
+  if [ -f "$WP_51_TESTS_DIR/.cache_hash" ] && [ "$WP_51_HASH" == "$(head -n 1 "$WP_51_TESTS_DIR/.cache_hash" )" ] && [ "$FORCE_DOWNLOAD" != true ]; then
     echo "WP 5.1 test suite has not changed; skipping..."
   else
-    [ -d $WP_51_TESTS_DIR ] && rm -rf $WP_51_TESTS_DIR
+    [ -d "$WP_51_TESTS_DIR" ] && rm -rf "$WP_51_TESTS_DIR"
 
-    mkdir -p $WP_51_TESTS_DIR
+    mkdir -p "$WP_51_TESTS_DIR"
 
     echo "Downloading WP 5.1 test suite..."
 
@@ -118,17 +118,17 @@ download_test_suites() {
 		  jq 'map( select( .name | startswith( "5.1" ) ) ) | first | .tarball_url' |\
 		  tr -d '"' |\
 		  xargs -n1 wget -O - -q |\
-		  tar --strip-components=1 -zx -C $WP_51_TESTS_DIR
+		  tar --strip-components=1 -zx -C "$WP_51_TESTS_DIR"
 
-	  echo $WP_51_HASH > $WP_51_TESTS_DIR/.cache_hash
+	  echo $WP_51_HASH > "$WP_51_TESTS_DIR/.cache_hash"
   fi
 
-  if [ -f $WP_LATEST_TESTS_DIR/.cache_hash ] && [ "$WP_LATEST_HASH" == "$(head -n 1 $WP_LATEST_TESTS_DIR/.cache_hash )" ]; then
+  if [ -f "$WP_LATEST_TESTS_DIR/.cache_hash" ] && [ "$WP_LATEST_HASH" == "$(head -n 1 "$WP_LATEST_TESTS_DIR/.cache_hash" )" ]; then
     echo "Latest WP test suite has not changed; skipping..."
   else
-    [ -d $WP_LATEST_TESTS_DIR ] && rm -rf $WP_LATEST_TESTS_DIR
+    [ -d "$WP_LATEST_TESTS_DIR" ] && rm -rf "$WP_LATEST_TESTS_DIR"
 
-    mkdir -p $WP_LATEST_TESTS_DIR
+    mkdir -p "$WP_LATEST_TESTS_DIR"
 
     echo "Downloading latest WP test suite..."
 
@@ -136,29 +136,29 @@ download_test_suites() {
 	    jq '.[0].tarball_url' |\
 	    tr -d '"' |\
 	    xargs -n1 wget -O - -q |\
-	    tar --strip-components=1 -zx -C $WP_LATEST_TESTS_DIR
+	    tar --strip-components=1 -zx -C "$WP_LATEST_TESTS_DIR"
 
-	  echo $WP_LATEST_HASH > $WP_LATEST_TESTS_DIR/.cache_hash
+	  echo $WP_LATEST_HASH > "$WP_LATEST_TESTS_DIR/.cache_hash"
   fi
 
 	echo "SUCCESS: test suites downloaded"
 }
 
 download_phpunit() {
-  if [ -f $PHPUNIT_DIR/phpunit4 ] && [ -f $PHPUNIT_DIR/phpunit5 ] && [ -f $PHPUNIT_DIR/phpunit6 ] && [ -f $PHPUNIT_DIR/phpunit7 ] && [ "$FORCE_DOWNLOAD" != true ]; then
+  if [ -f "$PHPUNIT_DIR/phpunit4" ] && [ -f "$PHPUNIT_DIR/phpunit5" ] && [ -f "$PHPUNIT_DIR/phpunit6" ] && [ -f "$PHPUNIT_DIR/phpunit7" ] && [ "$FORCE_DOWNLOAD" != true ]; then
     echo "PHPUnit has already been downloaded; skipping..."
 
     return
   fi
 
-  [ -d $PHPUNIT_DIR ] || mkdir -p $PHPUNIT_DIR
+  [ -d "$PHPUNIT_DIR" ] || mkdir -p "$PHPUNIT_DIR"
 
-  wget -O $PHPUNIT_DIR/phpunit4 https://phar.phpunit.de/phpunit-4.phar
-  wget -O $PHPUNIT_DIR/phpunit5 https://phar.phpunit.de/phpunit-5.phar
-  wget -O $PHPUNIT_DIR/phpunit6 https://phar.phpunit.de/phpunit-6.phar
-  wget -O $PHPUNIT_DIR/phpunit7 https://phar.phpunit.de/phpunit-7.phar
+  wget -O "$PHPUNIT_DIR/phpunit4" https://phar.phpunit.de/phpunit-4.phar
+  wget -O "$PHPUNIT_DIR/phpunit5" https://phar.phpunit.de/phpunit-5.phar
+  wget -O "$PHPUNIT_DIR/phpunit6" https://phar.phpunit.de/phpunit-6.phar
+  wget -O "$PHPUNIT_DIR/phpunit7" https://phar.phpunit.de/phpunit-7.phar
 
-  chmod +x $PHPUNIT_DIR/phpunit*
+  chmod +x "$PHPUNIT_DIR"/phpunit*
 
 	echo "SUCCESS: PHPUnit downloaded"
 }
@@ -168,17 +168,17 @@ download_gravityview() {
     _abort '"GH_AUTH_TOKEN" environment variable must be set to continue'
   fi
 
-  local GV_LATEST_HASH=$(wget --header="Authorization: token $GH_AUTH_TOKEN" -O - -q https://api.github.com/repos/gravityview/gravityview/tags | jq '.[0].commit.sha' | tr -d '"')
+  GV_LATEST_HASH=$(wget --header="Authorization: token $GH_AUTH_TOKEN" -O - -q https://api.github.com/repos/gravityview/gravityview/tags | jq '.[0].commit.sha' | tr -d '"')
 
-  if [ -f $GV_PLUGIN_DIR/.cache_hash ] && [ "$GV_LATEST_HASH" == "$(head -n 1 $GV_PLUGIN_DIR/.cache_hash )" ] && [ "$FORCE_DOWNLOAD" != true ]; then
+  if [ -f "$GV_PLUGIN_DIR/.cache_hash" ] && [ "$GV_LATEST_HASH" == "$(head -n 1 "$GV_PLUGIN_DIR/.cache_hash" )" ] && [ "$FORCE_DOWNLOAD" != true ]; then
     echo "Latest GravityView has already been downloaded; skipping..."
 
     return
   fi
 
-  [ -d $GV_PLUGIN_DIR ] && rm -rf $GV_PLUGIN_DIR
+  [ -d "$GV_PLUGIN_DIR" ] && rm -rf "$GV_PLUGIN_DIR"
 
-  mkdir -p $GV_PLUGIN_DIR
+  mkdir -p "$GV_PLUGIN_DIR"
 
   if [[ $1 == 'clone' ]]; then
     echo "Cloning GravityView repo..."
@@ -188,10 +188,10 @@ download_gravityview() {
       jq ".[0].tarball_url" |\
       tr -d '"' |\
       xargs -n1 wget --header="Authorization: token $GH_AUTH_TOKEN" -O - -q |\
-      tar --strip-components=1 -zx -C $GV_PLUGIN_DIR
+      tar --strip-components=1 -zx -C "$GV_PLUGIN_DIR"
   fi
 
-  echo $GV_LATEST_HASH > $GV_PLUGIN_DIR/.cache_hash
+  echo $GV_LATEST_HASH > "$GV_PLUGIN_DIR/.cache_hash"
 
 	[[ $1 == 'clone' ]] && echo "SUCCESS: GravityView repo cloned" || echo "SUCCESS: GravityView downloaded"
 }
@@ -201,17 +201,17 @@ download_gravity_forms() {
     _abort '"GH_AUTH_TOKEN" environment variable must be set to continue'
   fi
 
-  local GF_LATEST_HASH=$(wget --header="Authorization: token $GH_AUTH_TOKEN" -O - -q https://api.github.com/repos/gravityforms/gravityforms/tags | jq '.[0].commit.sha' | tr -d '"')
+  GF_LATEST_HASH=$(wget --header="Authorization: token $GH_AUTH_TOKEN" -O - -q https://api.github.com/repos/gravityforms/gravityforms/tags | jq '.[0].commit.sha' | tr -d '"')
 
-  if [ -f $GF_PLUGIN_DIR/.cache_hash ] && [ "$GF_LATEST_HASH" == "$(head -n 1 $GF_PLUGIN_DIR/.cache_hash )" ] && [ "$FORCE_DOWNLOAD" != true ]; then
+  if [ -f "$GF_PLUGIN_DIR/.cache_hash" ] && [ "$GF_LATEST_HASH" == "$(head -n 1 "$GF_PLUGIN_DIR/.cache_hash" )" ] && [ "$FORCE_DOWNLOAD" != true ]; then
     echo "Latest Gravity Forms has already been downloaded; skipping..."
 
     return
   fi
 
-  [ -d $GF_PLUGIN_DIR ] && rm -rf $GF_PLUGIN_DIR
+  [ -d "$GF_PLUGIN_DIR" ] && rm -rf "$GF_PLUGIN_DIR"
 
-  mkdir -p $GF_PLUGIN_DIR
+  mkdir -p "$GF_PLUGIN_DIR"
 
   echo "Downloading latest Gravity Forms..."
 
@@ -219,9 +219,9 @@ download_gravity_forms() {
     jq ".[0].tarball_url" |\
     tr -d '"' |\
     xargs -n1 wget --header="Authorization: token $GH_AUTH_TOKEN" -O - -q |\
-    tar --strip-components=1 -zx -C $GF_PLUGIN_DIR
+    tar --strip-components=1 -zx -C "$GF_PLUGIN_DIR"
 
-	echo $GF_LATEST_HASH > $GF_PLUGIN_DIR/.cache_hash
+	echo $GF_LATEST_HASH > "$GF_PLUGIN_DIR/.cache_hash"
 
 	echo "SUCCESS: Gravity Forms downloaded"
 }
@@ -234,35 +234,35 @@ prepare_all() {
 }
 
 test_54() {
-  _run_docker_compose "php5.4 $PHPUNIT_DIR/phpunit4 --no-coverage $1"
+  _run_docker_compose php5.4 "$PHPUNIT_DIR/phpunit4" --no-coverage $1
 }
 
 test_55() {
-  _run_docker_compose "php5.5 $PHPUNIT_DIR/phpunit4 --no-coverage $1"
+  _run_docker_compose php5.5 "$PHPUNIT_DIR/phpunit4" --no-coverage $1
 }
 
 test_56() {
-  _run_docker_compose "php5.6 $PHPUNIT_DIR/phpunit5 --no-coverage $1"
+  _run_docker_compose php5.6 "$PHPUNIT_DIR/phpunit5" --no-coverage $1
 }
 
 test_70() {
-  _run_docker_compose "php7.0 $PHPUNIT_DIR/phpunit6 --no-coverage $1"
+  _run_docker_compose php7.0 "$PHPUNIT_DIR/phpunit6" --no-coverage $1
 }
 
 test_71() {
-  _run_docker_compose "php7.1 $PHPUNIT_DIR/phpunit7 --no-coverage $1"
+  _run_docker_compose php7.1 "$PHPUNIT_DIR/phpunit7" --no-coverage $1
 }
 
 test_72() {
-  _run_docker_compose "php7.2 $PHPUNIT_DIR/phpunit7 --no-coverage $1"
+  _run_docker_compose php7.2 "$PHPUNIT_DIR/phpunit7" --no-coverage $1
 }
 
 test_73() {
-  _run_docker_compose "php7.3 $PHPUNIT_DIR/phpunit7 --no-coverage $1"
+  _run_docker_compose php7.3 "$PHPUNIT_DIR/phpunit7" --no-coverage $1
 }
 
 test_74() {
-  _run_docker_compose "php7.4 $PHPUNIT_DIR/phpunit7 --no-coverage $1"
+  _run_docker_compose php7.4 "$PHPUNIT_DIR/phpunit7" --no-coverage $1
 }
 
 test_all() {
@@ -271,8 +271,8 @@ test_all() {
   test_56 "$1"
   test_70 "$1"
   test_71 "$1"
-  test_73 "$1"
   test_72 "$1"
+  test_73 "$1"
   test_74 "$1"
 }
 
